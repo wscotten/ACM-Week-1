4b8261ff38a3543d94c6a74580f04dc8c7df2984
William <wscotten@dhcp-v075-026.mobile.uci.edu>
2017-10-24 11:17:20 -0700

    removed return

--- temp.js
+++ temp.js
@@ -1,17 +1,17 @@
-const uniquePathsWithObstacles = (obstacleGrid) => 
-  obstacleGrid.reduce((acc, row, i) => {
-    return [...acc, row.reduce((rowAcc, initialValue, j) => {
-      if(i === 0 && j === 0)
-        return initialValue === 0 ? [1] : [0];
-      else if(obstacleGrid[i][j] === 1)
-        return [...rowAcc, 0];
-      else {
-        let counter = 0;
-        if (i > 0)
-          counter += acc[i - 1][j];
-        if (j > 0)
-          counter += rowAcc[j - 1];
-        return [...rowAcc, counter];
-      }
-    }, [])]
-  }, [])[obstacleGrid.length - 1][obstacleGrid[0].length - 1]
+  const uniquePathsWithObstacles = (obstacleGrid) => 
+    obstacleGrid.reduce((acc, row, i) => 
+      [...acc, row.reduce((rowAcc, initialValue, j) => {
+        if(i === 0 && j === 0)
+          return initialValue === 0 ? [1] : [0];
+        else if(obstacleGrid[i][j] === 1)
+          return [...rowAcc, 0];
+        else {
+          let counter = 0;
+          if (i > 0)
+            counter += acc[i - 1][j];
+          if (j > 0)
+            counter += rowAcc[j - 1];
+          return [...rowAcc, counter];
+        }
+      }, [])
+    ], [])[obstacleGrid.length - 1][obstacleGrid[0].length - 1]
